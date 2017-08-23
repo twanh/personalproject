e6b4c126088e2d3e44e6bfba92390f86f5995b9e
twanh <huiskenstwan@gmail.com>
2017-08-23 13:48:02 +0200

    Update .gitignore to include vscode folder

--- webcrawler/crawler.py
+++ webcrawler/crawler.py
@@ -1,43 +1,109 @@
-import requests
-from bs4 import BeautifulSoup
 import json
 
-# GTA TEST
+import requests
 
-query = 'gta'
-url = 'https://g2a.com/lucene/search/filter?jsoncallback=&skip=&minPrice=0.00&maxPrice=1422.00&cc=NL&stock=all&event=&search={}&genre=0&cat=0&sortOrder=popularity+desc&start=0&rows=12&steam_app_id=&steam_category=&steam_prod_type=&includeOutOfStock=false&includeFreeGames=false&isWholesale=false&_=1503469082372'.format(query)
-
-req = requests.get(url)
-
-print('Status Code: {}'.format(req.status_code))
-print('-' * 50, end='\n')
-# print(req.text)
-
-# Remove weird ()
-data = req.text[1:-1]
-
-json_data = json.loads(data)
-
-# with open('testfile.json', 'w') as f:
-#     f.write(json.dumps(json_data))
+class CrawlRequestError(Exception):
+    ''' 
+    Raise when something goes wrong while crawling a page
+    specificly when the request goes wrong. 
+    '''
+    pass
 
 
-num_found = json_data['numFound']
-print(num_found)
-print('-' * 50, end='\n')
 
-docs = json_data['docs']
+class G2a:
+    '''Webcrawler for g2a.com'''
 
-results = []
+    def game(self, url):
+        '''
+        Crawl the game's page (url) and extract relevant information to display on the site
 
-for doc in docs:
-    game_name = doc['name']
-    price = doc['minPrice']
-    slug = doc['slug']
-    small_img = doc['smallImage']
+        Args:
+            url: (str) The url of the game
+        Returns:
+            (dict) The relevant information
+        Raises:
 
-    print('Name: {}'.format(game_name))
-    print('Price: {}'.format(price))
-    print('Slug: {}'.format(slug))
-    print('Img: {}'.format(small_img))
-    print('-' * 50, end='\n')
+        '''
+
+    def search(self, query):
+        '''
+        Crawl g2a search to return the results of the search
+
+        Args:
+            query: (str) Search query
+        Returns:
+            (list) The results
+        Raises:
+            None
+        '''
+
+        # This list is used to store the results
+        # This list is returend at the end of this function
+        results = []
+
+        # Combine the user specified search query with the already predifinced url of g2a search api
+        url = 'https://g2a.com/lucene/search/filter?jsoncallback=&skip=&minPrice=0.00&maxPrice=1422.00&cc=NL&stock=all&event=&search={}&genre=0&cat=0&sortOrder=popularity+desc&start=0&rows=12&steam_app_id=&steam_category=&steam_prod_type=&includeOutOfStock=false&includeFreeGames=false&isWholesale=false&_=1503469082372'.format(query)
+
+        # We use requests to 'request' the url and therefore return the content of the search
+        r = requests.get(url)
+
+        # We check if the request wa sucessfull,
+        # If it was sucess full we create the data variable which holds the returned content
+        if r.status_code == requests.codes.ok:
+
+            # We coould use r.json but we dont 
+            # because the api adds () to the response
+            # because of this same reason we remove the first and last character of the response data
+            data = r.text[1:-1]
+
+        else:
+            data = ''
+            raise CrawlRequestError('Request had code: {} and failed'.format(r.status_code))
+
+        #  Convert the string from r.text to a python interpretable dict
+        json_data = json.loads(data)
+        
+        # Extract the number of results form the data
+        num_found = json_data['numFound']
+
+        # Extract the actual games from the results
+        # By looping over all the games 
+        # For everygame we extract the name, price, slug and the small_img
+        # This gets combined in a new dictionary that gets added to the results list
+        games = json_data['docs'] 
+        for game in games:
+            game_name = game['name']
+            price = game['minPrice']
+            slug = game['slug']
+            url = 'https://g2a.com{}'.format(slug)
+            small_img = game['smallImage']
+
+            current_game = {
+                'name': game_name,
+                'price': price,
+                'url': url,
+                'small_img': small_img
+                }
+            
+            results.append(current_game)
+
+        return results
+
+
+'''
+# Testing
+g3a = G2a()
+s = g3a.search('gta')
+print('-'*10, end='\n')
+print('Type: {}'.format(type(s)))
+print('-'*10, end='\n')
+print('List: ', end='\n')
+print(str(s))
+print('-'*10, end='\n')
+for game in s:
+    print(game['name'])
+    print(game['price'])
+    print(game['url'])
+    print('-'*10)
+'''
