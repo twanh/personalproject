f6efaf732585292c1479b7de253cfa85b0ddf7f4
twanh <huiskenstwan@gmail.com>
2017-08-28 14:31:18 +0200

    Simple tests for gamemangers

--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from games.models import GameManager
 
-# Create your tests here.
+# Simply Test GameManager
+def test_gamemanager():
+    gm = GameManager()
+    name = 'Grand Theft Auto V'
+    urls = {'g2a':'https://www.g2a.com/grand-theft-auto-v-rockstar-key-global-i10000000788017',
+            'kinguin': 'https://www.kinguin.net/category/15836/grand-theft-auto-v-rockstar-digital-download-key/'}
+    
+    created = gm._create_from_crawl(name, urls)
