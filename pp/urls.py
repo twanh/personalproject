a3137b52faac6d97c8e4ce53cd377031ffc5bf4f
twanh <huiskenstwan@gmail.com>
2017-08-28 11:20:11 +0200

    Created a simple temporary index page

--- pp/urls.py
+++ pp/urls.py
@@ -19,5 +19,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='_layouts/_base.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 ]
