a9b40e4b98e162933b90d6951fd330c2369b5d06
twanh <huiskenstwan@gmail.com>
2017-08-28 12:17:38 +0200

    Remove sellers models for now

--- sellers/models.py
+++ sellers/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-class Seller(models.Model):
-    name = models.CharField(max_lenght=50)
-    search_url = models.URLField()
-    game_url = models.URLField()
-    logo_url = models.URLField()
-    rating = models.IntegerField()
+# class Seller(models.Model):
+#     name = models.CharField(max_lenght=50)
+#     search_url = models.URLField()
+#     game_url = models.URLField()
+#     logo_url = models.URLField()
+#     rating = models.IntegerField()
