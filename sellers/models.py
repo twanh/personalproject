e901d193741541cb81c0ec9308c612d5872f2a20
twanh <huiskenstwan@gmail.com>
2017-09-09 16:17:53 +0200

    Add reflink

--- sellers/models.py
+++ sellers/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class Seller(models.Model):
     ''' Model for sellers '''
-    name = models.CharField(max_length=50)
-    search_url = models.URLField()
-    logo_url = models.URLField()
-    rating = models.CharField(max_length=5)
+    name        = models.CharField(max_length=50)
+    search_url  = models.URLField()
+    ref_url     = models.URLField()
+    logo_url    = models.URLField()
+    rating      = models.CharField(max_length=5)
 
     def __str__(self):
         return self.name
