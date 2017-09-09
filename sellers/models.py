a01022bce49eeb3032f649fca843abe0c32589bc
twanh <huiskenstwan@gmail.com>
2017-09-09 16:04:25 +0200

    Created model of seller

--- sellers/models.py
+++ sellers/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+class Seller(models.Model):
+    ''' Model for sellers '''
+    name = models.CharField(max_lenght=50)
+    search_url = models.URLField()
+    logo_url = models.URLField()
+    rating = models.CharField(max_lenght=5)
 
-# class Seller(models.Model):
-#     name = models.CharField(max_lenght=50)
-#     search_url = models.URLField()
-#     game_url = models.URLField()
-#     logo_url = models.URLField()
-#     rating = models.IntegerField()
