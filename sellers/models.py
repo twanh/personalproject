19ce71bb63e42ab4f6a151851755ab8690e5eebe
twanh <huiskenstwan@gmail.com>
2017-09-09 16:05:57 +0200

    Spelling errors fixed

--- sellers/models.py
+++ sellers/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Seller(models.Model):
     ''' Model for sellers '''
-    name = models.CharField(max_lenght=50)
+    name = models.CharField(max_length=50)
     search_url = models.URLField()
     logo_url = models.URLField()
-    rating = models.CharField(max_lenght=5)
+    rating = models.CharField(max_length=5)
 
