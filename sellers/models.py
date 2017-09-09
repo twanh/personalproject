f7a4db417c99002afeac906f2ec05a95829ea288
twanh <huiskenstwan@gmail.com>
2017-09-09 16:16:21 +0200

    Add str method

--- sellers/models.py
+++ sellers/models.py
@@ -5,5 +5,7 @@ class Seller(models.Model):
     name = models.CharField(max_length=50)
     search_url = models.URLField()
     logo_url = models.URLField()
-    rating = models.CharField(max_length=5)
+    rating = models.CharField(max_lenght=5)
 
+    def __str__(self):
+        return self.name
